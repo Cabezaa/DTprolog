4d56066814300a5a00f7c7e2c5e3d688e41eb838
Esteban Ruiz de Galarreta <cabeza180@gmail.com>
2017-06-16 22:51:29 -0300

    Mejorada la Estructura del proyecto. Agregado los scripts y DATA-TEST

--- Datos/scriptFil2.py
+++ Datos/scriptFil2.py
@@ -2,12 +2,18 @@
 #14 atributos + 1 clasificaicon!
 def main():
     nuevaLinea = ""
+    escritura = ""
+    bandera = True
     i = 0
-    archivoEntrada = open("entrada6000lineas.txt", "r")
-    archivoSalida = open("filtradoConNuestrosAtributosWEKA.txt","r+")
+    cantLin = 0
+    archivoEntrada = open("test3Entrada.txt", "r")
+    archivoSalida = open("test3Salida.txt","r+")
     print("Antes de entrar al for")
     for linea in archivoEntrada.readlines():
+        cantLin = cantLin + 1
         arreglo = linea.split(',')
+        nuevaLinea = ""
+        bandera = True
         #nuevaLinea = nuevaLinea + '('
         for i in range(0, 14):
             if i==13:
@@ -30,37 +36,48 @@ def main():
             elif i==11:
                 x = int(arreglo[i])
                 if x == 0:
-                    tieneInversiones = "si"
-                else:
                     tieneInversiones = "no"
+                else:
+                    tieneInversiones = "si"
                 nuevaLinea = nuevaLinea +tieneInversiones+', '
             elif i==10:
                 x = int(arreglo[i])
                 if x == 0:
-                    tieneInversiones = "si"
-                else:
                     tieneInversiones = "no"
+                else:
+                    tieneInversiones = "si"
                 nuevaLinea = nuevaLinea + tieneInversiones+', '
             elif i==9:
                 nuevaLinea = nuevaLinea +arreglo[i]+', '
             elif i==8:
                 if(arreglo[i] != " Amer-Indian-Eskimo"):
                     nuevaLinea = nuevaLinea + arreglo[i]+', '
+                else:
+                    bandera = False
             elif i==7:
                 nuevaLinea = nuevaLinea +arreglo[i]+', '
             elif i==6:
                 if(arreglo[i] == " Adm-clerical") or (arreglo[i] == " Craft-repair") or (arreglo[i] == " Exec-managerial") or (arreglo[i] == " Machine-op-inspct") or (arreglo[i] == " Other-service") or (arreglo[i] == " Tech-support") or (arreglo[i] == " Prof-specialty") or (arreglo[i] == " Sales") or (arreglo[i] == " Handlers-cleaners"):
                     nuevaLinea = nuevaLinea + arreglo[i]+', '
+                else:
+                    bandera = False
             elif i==5:
                 if(arreglo[i] == " Divorced") or (arreglo[i] == " Married-civ-spouse") or (arreglo[i] == " Never-married") or (arreglo[i] == " Separated"):
                     nuevaLinea = nuevaLinea +arreglo[i]+', '
+                else:
+                    bandera = False
+                    print("entre matrimonio false")
             elif i==3:
                 if(arreglo[i] == " HS-grad") or (arreglo[i] == " Some-college") or (arreglo[i] == " Bachelors") or (arreglo[i] == " Masters") or (arreglo[i] == " Doctorate"):
                     nuevaLinea = nuevaLinea + arreglo[i]+', '
+                else:
+                    bandera = False
 
             elif i==1:
                 if (arreglo[i] == " Federal-gov") or (arreglo[i] == " Private") or (arreglo[i] == " Self-emp-not-inc") or (arreglo[i] == " State-gov"):
                     nuevaLinea = nuevaLinea + arreglo[i]+', '
+                else:
+                    bandera = False
             elif i == 0:
                 x = int(arreglo[i])
                 if x < 25:
@@ -79,9 +96,14 @@ def main():
         if arreglo[14] == " >50K\n":
             nuevaLinea = nuevaLinea + 'y\n'
 
-    print("antes de escribir")
-    archivoSalida.write(nuevaLinea)
-    print("despues de escribir")
+        if (bandera == True):
+            escritura = escritura + nuevaLinea
+            #print("Linea numero: " + str(cantLin) + " es: \n")
+            #print(linea)
+            #print('\n')
+            #print(nuevaLinea)
+
+    archivoSalida.write(escritura)
 
 
 main()
